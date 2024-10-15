69d17563f19118e2d0a433e703dd8afe65acdad9
kisernl <noah.kiser@gmail.com>
2024-10-15 15:30:40 -0500

    add target to footer A tags

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ import Github from '../images/github-icon.svg';
 function Footer() {
   return (
     <footer>
-      <a href="https://substack.com/@noahkiser">
+      <a href="https://substack.com/@noahkiser" target='_blank'>
         <img src={Substack} alt="substack icon" />
       </a>
-      <a href="https://github.com/kisernl">
+      <a href="https://github.com/kisernl" target='_blank'>
         <img src={Github} alt="github icon" />
       </a>
     </footer>
